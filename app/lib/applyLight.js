d61a454c0baa98f0b5540c00541dabc9389ba76c
DESKTOP-MEA5A46\ST <developefullstack420@gmail.com>
2024-10-14 03:42:36 -0400

    fix light

--- app/lib/applyLight.js
+++ app/lib/applyLight.js
@@ -10,6 +10,7 @@ import {
   CubeTexture,
   MeshBuilder,
   StandardMaterial,
+  SpotLight,
 } from "@babylonjs/core";
 
 let light;
@@ -21,6 +22,7 @@ export default function applyLight(engine, scene) {
   applyFogEffect(scene);
   applyEnvironmentTexture(scene);
   addPointLights(scene);
+  addSportLights(scene);
 }
 
 function addDirectionalLight(scene) {
@@ -56,14 +58,60 @@ function applyEnvironmentTexture(scene) {
   scene.environmentTexture = CubeTexture.CreateFromPrefilteredData("/environment.dds", scene);
 }
 
+// Add interior spotlights
+function addSportLights(scene) {
+
+  const spotLight1 = new SpotLight(
+    "spotLight1",
+    new Vector3(0, 30, 5),
+    new Vector3(0, -1, 0),
+    Math.PI / 100,
+    2,
+    scene
+  );
+  spotLight1.intensity = 0.8;
+}
+
+
 function addPointLights(scene) {
+
+  const spotLight1 = new SpotLight(
+    "spotLight1",
+    new Vector3(0, -5, 0),
+    new Vector3(0, 20, 0),
+    Math.PI / 1,
+    2,
+    scene
+  );
+  spotLight1.intensity = 0.6;
+
+  // const spotLight2 = new SpotLight(
+  //   "spotLight2",
+  //   new Vector3(-50, 0, 0),
+  //   new Vector3(300, 0, 0),
+  //   Math.PI / 1,
+  //   2,
+  //   scene
+  // );
+  // spotLight2.intensity = 0.3;
+
+  // const spotLight3 = new SpotLight(
+  //   "spotLight3",
+  //   new Vector3(5, 0, 0),
+  //   new Vector3(-300, 0, 0),
+  //   Math.PI / 10,
+  //   2,
+  //   scene
+  // );
+  // spotLight3.intensity = 0.5;
+
   // Define point lights with their respective positions and properties
   const pointLights = [
     // { name: 'pointLight1', position: new Vector3(-1.6, 2.7, 0), intensity: 0.3, range: 10 },
     // { name: 'pointLight2', position: new Vector3(0.6, 2.7, 1), intensity: 0.3, range: 10 },
-    { name: 'pointLight3', position: new Vector3(3, 2.7, 0), intensity: 0.4, range: 50 },
-    { name: 'pointLight4', position: new Vector3(-3, 2.7, 0), intensity: 0.4, range: 50 },
-    { name: 'pointLight5', position: new Vector3(0, 2.7, 0), intensity: 0.4, range: 50 },
+    { name: 'pointLight3', position: new Vector3(0, 2.7, 0), intensity: 0.3, range: 500 },
+    // { name: 'pointLight4', position: new Vector3(-3, 2.7, 0), intensity: 0.4, range: 500 },
+    // { name: 'pointLight5', position: new Vector3(0, 2.7, 0), intensity: 0.4, range: 500 },
   ];
 
   pointLights.forEach(lightInfo => {
@@ -74,13 +122,13 @@ function addPointLights(scene) {
     pointLight.range = lightInfo.range;
 
     // Create a small sphere to visually represent the light position
-    const lightSphere = MeshBuilder.CreateSphere(`${lightInfo.name}Sphere`, { diameter: 0.1 }, scene);
-    lightSphere.position = lightInfo.position.clone();
+    // const lightSphere = MeshBuilder.CreateSphere(`${lightInfo.name}Sphere`, { diameter: 0.1 }, scene);
+    // lightSphere.position = lightInfo.position.clone();
 
     // Assign a standard material to the sphere
-    const material = new StandardMaterial(`${lightInfo.name}Material`, scene);
-    material.emissiveColor = new Color3(1, 1, 0); // Yellow to simulate light glow
-    lightSphere.material = material;
+    // const material = new StandardMaterial(`${lightInfo.name}Material`, scene);
+    // material.emissiveColor = new Color3(1, 1, 0); // Yellow to simulate light glow
+    // lightSphere.material = material;
   });
 }
 
